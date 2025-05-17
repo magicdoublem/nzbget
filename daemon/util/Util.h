275eae9a7e8bf7a16899ab5c531b79ad218f98ba
magicdoublem <48841961+magicdoublem@users.noreply.github.com>
2025-05-18 00:47:04 +0200

    cleanup tests

--- daemon/util/Util.h
+++ daemon/util/Util.h
@@ -208,7 +208,7 @@ public:
 class WebUtil
 {
 public:
-	static int DecodeBase64(char* inputBuffer, int inputBufferLength, char* outputBuffer);
+	static uint32 DecodeBase64(char* inputBuffer, int inputBufferLength, char* outputBuffer);
 
 	/*
 	* Encodes string to be used as content of xml-tag.
