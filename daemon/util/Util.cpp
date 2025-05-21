b884da01dfc573ba9f57abfdc39d8696d4feb0e7
magicdoublem <48841961+magicdoublem@users.noreply.github.com>
2025-05-21 20:14:20 +0200

    Update Util.cpp

--- daemon/util/Util.cpp
+++ daemon/util/Util.cpp
@@ -893,7 +893,7 @@ uint32 WebUtil::DecodeBase64(char* inputBuffer, int inputBufferLength, char* out
 	uint32 InputBufferLength = inputBufferLength > 0 ? inputBufferLength : strlen(inputBuffer);
 
 	char ByteQuartet [4];
-	int32 i = 0;
+	int i = 0;
 	while (InputBufferIndex < InputBufferLength)
 	{
 		// Ignore all characters except the ones in BASE64_ALPHABET
